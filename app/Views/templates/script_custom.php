eb517bc5267b0a8293dda69766f69b70d8d93374
Dany Prastio <danyprastio14@gmail.com>
2024-12-15 20:59:37 +0700

    fix: setup lib to import local

--- app/Views/templates/script_custom.php
+++ app/Views/templates/script_custom.php
@@ -4,15 +4,15 @@
 <script src="<?= base_url() ?>assets/js/vue/app.constants.js" type="text/javascript"></script>
 
 <!-- Datatables Cdn -->
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.6/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="<?= base_url() ?>assets/js/libs/jquery.dataTables.min.js"></script>
+<script src="<?= base_url() ?>assets/js/libs/dataTables.bootstrap5.min.js"></script>
+<script src="<?= base_url() ?>assets/js/libs/dataTables.responsive.min.js"></script>
+<script src="<?= base_url() ?>assets/js/libs/dataTables.buttons.min.js"></script>
+<script src="<?= base_url() ?>assets/js/libs/buttons.print.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.6/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script> -->
+<script src="<?= base_url() ?>assets/js/libs/buttons.html5.min.js"></script>
+<script src="<?= base_url() ?>assets/js/libs/jszip.min.js"></script>
 
  <!-- Date & Time Picker JS -->
  <script src="<?= base_url() ?>assets/libs/flatpickr/flatpickr.min.js"></script>
