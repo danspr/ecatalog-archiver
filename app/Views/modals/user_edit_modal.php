11a0afe0a2af1b481fd4f68b8b856b86e157414b
Dany Prastio <danyprastio14@gmail.com>
2025-01-27 20:49:42 +0700

    fix: edit modal text

--- app/Views/modals/user_edit_modal.php
+++ app/Views/modals/user_edit_modal.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h6 class="modal-title">New User
+                <h6 class="modal-title">Edit User
                 </h6>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                     aria-label="Close"></button>
@@ -34,7 +34,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" @click="updateUser" id="createButton">Create</button>
+                <button type="button" class="btn btn-primary" @click="updateUser" id="createButton">Save</button>
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
